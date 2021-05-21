30960685f7598fe8c26b0299268e800e2fe18f22
Diogo Simões <dsimoes2000@gmail.com>
2021-05-21 06:08:18 +0100

    Adds clock.py documentation

--- src/utils/clock.py
+++ src/utils/clock.py
@@ -3,16 +3,40 @@ import time
 class Clock:
     @staticmethod
     def now():
+        """
+        Returns:
+            float: current time
+        """        
         return time.time()
 
     @staticmethod
     def isAfter(date_curr, date_next):
+        """
+        Compares times.
+
+        Args:
+            - date_curr (float)
+            - date_next (float)
+
+        Returns:
+            bool: True if date_curr after date_next
+        """        
         if date_curr is None or date_next is None:
             return False
         return date_curr > date_next
 
     @staticmethod
     def addSeconds(date_curr, seconds):
+        """
+        Returns the given time + given seconds.
+
+        Args:
+            date_curr (float): Given Dates
+            seconds (int): Seconds
+
+        Returns:
+            float: date_curr[0] + seconds
+        """        
         if date_curr is None:
             return False
         return date_curr[0] + seconds
