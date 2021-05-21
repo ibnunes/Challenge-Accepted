e916ebb0f5e632c37cebca0d08aaebf2b3755089
Diogo Simões <dsimoes2000@gmail.com>
2021-05-21 22:47:29 +0100

    Completes setup.py files

--- src/client/setup.py
+++ src/client/setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup, find_packages
 
 setup(
-    name='Challenge Accepted',
+    name='Challenge Accepted CLI',
     description='System to Publish Cryptographic Challenges',
     long_description='Encryption contains excellent tools for developing challenging mystery games. \
 The idea of this work is to develop a system that allows different users to publish and solve challenges.',
-    version='1.0.0',
+    version='1.0.0-beta',
     author='C-Team',
+    license='GNU-GPL 3.0',
     install_requires=[
         'Padding',
         'prettytable',
--- src/server/setup.py
+++ src/server/setup.py
@@ -1,18 +1,16 @@
 from distutils.core import setup, find_packages
 
 setup(
-    name='Challenge Accepted',
+    name='Challenge Accepted Server',
     description='System to Publish Cryptographic Challenges',
     long_description='Encryption contains excellent tools for developing challenging mystery games. \
 The idea of this work is to develop a system that allows different users to publish and solve challenges.',
-    version='1.0.0',
+    version='1.0.0-beta',
     author='C-Team',
+    license='GNU-GPL 3.0',
     install_requires=[
-        'Padding',
-        'prettytable',
-        'pycryptodome',
-        'requests',
-        'validate_email'
+        'flask',
+        'mariadb'
     ],
-    packages=find_packages(),
+    packages=find_packages()
 )
